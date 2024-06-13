1876a841c7ae4a5533f2437cc31bf73d68e32eb9
chaima garoui <chaimagaroui2@gmail.com>
2024-06-13 15:09:29 +0200

    display diffrents values of a fariable

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //document.getElementById("count").innerText = 5
 
 
-let count = 0
-
+let count = 5
+count = count + 1
 console.log(count)
 
-//creer ma variable age et lui accorder une valeur 
-let MyAge = 35 
+//create a variable , bonuspoints, initialize it as 50.I ncrease it to 100
+//decrease it to 25, and then finally increase it to 70 
+//console.log The value after each step 
 
+let bonuspoints =50
+console.log(bonuspoints)
+
+bonuspoints = bonuspoints - 75
+console.log(bonuspoints)
+
+bonuspoints = bonuspoints + 45
+console.log(bonuspoints)
